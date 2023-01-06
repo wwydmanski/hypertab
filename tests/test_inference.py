c9d7c85dd4491cf352687af9808bb6abfbdd1e8b
Witold Wydmański <wwydmanski@wwydmanski-gpu.mcb.bio>
2023-01-06 11:35:36 +0100

    fix: requirements

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -6,7 +6,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 sys.path.append('../')
-from tabular_hypernet.hypernetwork import Hypernetwork
+from hypertab.hypernetwork import Hypernetwork
 
 def test_inference():
     np.random.seed(42)
