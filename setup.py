8d8c82a5b5868a8bc60c0b8249f19b6dcaf40f90
Witold Wydmański <wwydmanski@wwydmanski-gpu.mcb.bio>
2023-01-06 11:32:30 +0100

    chore: requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="hypertab",
     version="0.1.0",
     description="TBD",
     packages=find_packages(include=['hypertab', 'hypertab.*']),
-    install_requires=["numpy >= 1.11.1", "matplotlib >= 1.5.1"],
+    install_requires=required,
 )
