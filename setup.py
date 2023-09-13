31638d0ad870ee9b44d0f6ebcf5b94c76cea77a0
Witold Wydmański <wwydmanski@LS-Debian11-Neon-mDF.mcb.bio>
2023-09-13 04:14:54 -0400

    fix: remove git headings

--- setup.py
+++ setup.py
@@ -11,11 +11,7 @@ setup(
     name="hypertab",
     author="Witold Wydmański",
     author_email="wwydmanski@gmail.com",
-<<<<<<< HEAD
-    version="0.2.2",
-=======
-    version="0.3.2",
->>>>>>> parent of 4723af0 (feat: optimizations)
+    version="0.6.2",
     description="HyperTab: hypernetwork for small tabular datasets",
     long_description=long_description,
     long_description_content_type='text/markdown',
